04dbd93e923605cca0f7bdf5c6c1b15f06c3addc
Aryan Kapoor <rnkap97@gmail.com>
2020-04-16 16:39:10 +0530

    final version

---
+++ confusion/components/ReservationComponent.js
@@ -0,0 +1,232 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  Picker,
+  Switch,
+  Button,
+  Alert
+} from "react-native";
+
+import { Card } from "react-native-elements";
+import DatePicker from "react-native-datepicker";
+import * as Animatable from 'react-native-animatable';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import * as Calendar from 'expo-calendar';
+
+class Reservation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      guests: 1,
+      smoking: false,
+      date: "",
+      showModal: false
+    };
+  }
+
+  static navigationOptions = {
+    title: "Reserve Table"
+  };
+
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+  confirmReservation(date) {
+    this.presentLocalNotification(date);
+    this.addReservationToCalendar(date);
+    this.resetForm();
+  }
+
+  handleReservation() {
+    // console.log(JSON.stringify(this.state));
+    // this.toggleModal();
+    const { date, guests, smoking } = this.state;
+      Alert.alert(
+          'Your reservation OK?',
+          'Your reservation guest:'+this.state.guests+"\n"+'Smoking:'+this.state.smoking+'\n'+'Date of the time:'+this.state.date,
+          // 'Smoking:'+this.state.smoking?'true':'false'+
+          // 'Date of the time:'+this.state.date,
+          [
+          {text: 'Cancel', onPress: () => this.resetForm(), style: 'cancel'},
+          {text: 'OK', onPress: () => {this.confirmReservation(date)}},
+          ],
+          { cancelable: false }
+      );
+
+  }
+
+  resetForm() {
+    this.setState({
+      guests: 1,
+      smoking: false,
+      date: "",
+      showModal: false
+    });
+  }
+
+  async obtainNotificationPermission() {
+    let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+    if (permission.status !== 'granted') {
+        permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            Alert.alert('Permission not granted to show notifications');
+        }
+    }
+    return permission;
+}
+static async obtainCalendarPermission() {
+  let permission = await Permissions.getAsync(Permissions.CALENDAR);
+  if (permission.status !== 'granted') {
+    permission = await Permissions.askAsync(Permissions.CALENDAR);
+    if (permission.status !== 'granted') {
+      Alert.alert('Permission not granted to access the calendar');
+    }
+  }
+  return permission;
+}
+
+async presentLocalNotification(date) {
+    await this.obtainNotificationPermission();
+    Notifications.presentLocalNotificationAsync({
+        title: 'Your Reservation',
+        body: 'Reservation for '+ date + ' requested',
+        ios: {
+            sound: true
+        },
+        android: {
+            sound: true,
+            vibrate: true,
+            color: '#512DA8'
+        }
+    });
+  }
+
+  async obtainDefaultCalendarId() {
+    let calendar = null;
+    if (Platform.OS === 'ios') {
+      // ios: get default calendar
+      calendar = await Calendar.getDefaultCalendarAsync();
+    } else {
+      // Android: find calendar with `isPrimary` == true
+      const calendars = await Calendar.getCalendarsAsync();
+      calendar = (calendars) ?
+        (calendars.find(cal => cal.isPrimary) || calendars[0])
+        : null;
+    }
+    return (calendar) ? calendar.id : null;
+  }
+
+   async addReservationToCalendar(date) {
+    await Reservation.obtainCalendarPermission();
+    const calendars = await this.obtainDefaultCalendarId();
+    const startDate = new Date(Date.parse(date));
+    const endDate = new Date(Date.parse(date) + (2 * 60 * 60 * 1000)); // 2 hours
+    Calendar.createEventAsync(
+      calendars,
+      {
+        title: 'Con Fusion Table Reservation',
+        location: '121, Clear Water Bay Road, Clear Water Bay, Kowloon, Hong Kong',
+        startDate,
+        endDate,
+        timeZone: 'Asia/Hong_Kong',
+      },
+    );
+    Alert.alert('Reservation has been added to your calendar');
+  }
+
+  render() {
+    const todayDate = new Date().toISOString().split('T')[0];
+    return (
+      <Animatable.View animation="zoomIn" duration={2000}>
+      <ScrollView>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>Number of Guests</Text>
+          <Picker
+            style={styles.formItem}
+            selectedValue={this.state.guests}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ guests: itemValue })
+            }
+          >
+            <Picker.Item label="1" value="1" />
+            <Picker.Item label="2" value="2" />
+            <Picker.Item label="3" value="3" />
+            <Picker.Item label="4" value="4" />
+            <Picker.Item label="5" value="5" />
+            <Picker.Item label="6" value="6" />
+          </Picker>
+        </View>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
+          <Switch
+            style={styles.formItem}
+            value={this.state.smoking}
+            onTintColor="#512DA8"
+            onValueChange={value => this.setState({ smoking: value })}
+          />
+        </View>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>Date and Time</Text>
+          <DatePicker
+            style={{ flex: 2, marginRight: 20 }}
+            date={this.state.date}
+            format=""
+            mode="datetime"
+            placeholder="select date and Time"
+            minDate={todayDate}
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: "absolute",
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+              // ... You can check the source to find the other keys.
+            }}
+            onDateChange={date => {
+              this.setState({ date: date });
+            }}
+          />
+        </View>
+        <View style={styles.formRow}>
+          <Button
+            onPress={() => this.handleReservation()}
+            title="Reserve"
+            color="#512DA8"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+      </ScrollView>
+      </Animatable.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  formRow: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+    margin: 20
+  },
+  formLabel: {
+    fontSize: 18,
+    flex: 2
+  },
+  formItem: {
+    flex: 1
+  }
+});
+
+export default Reservation;
---
+++ json-server/readme.txt
@@ -0,0 +1,7 @@
+this folder contains the production build of conFusion resturant website made using react.
+
+To run the project:
+
+json-server --host 192.168.31.56 db.json -p 3001 -d 200
+
+and the open localhost:3001 in browser
